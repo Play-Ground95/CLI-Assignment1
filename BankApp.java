4caa631bf5e133d0eb32d35b488f89ca34b22385
Kavindu95 <kavindueng95@gmail.com>
2023-08-15 22:56:57 +0530

    color added

--- BankApp.java
+++ BankApp.java
@@ -1,5 +1,29 @@
 public class BankApp{
     public static void main(String[] args) {
-        
+        final String CLEAR = "\033[H\033[2J";
+        final String COLOR_BLUE_BOLD = "\033[34;1m";
+        final String COLOR_RED_BOLD = "\033[31;1m";
+        final String COLOR_GREEN_BOLD = "\033[33;1m";
+        final String RESET = "\033[0m";
+
+        final String DASHBOARD = "👷 Welcome To Smart Banking System";
+        final String ADD_ACCOUNT = "➕ Add New Account";
+        final String DELETE_ACCOUNT = "❌ Remove Exisiting Account";
+        final String PRINT_BALANCE = "🖨 Print Balance";
+
+        final String ERROR_MSG = String.format("\t%s%s%s\n", COLOR_RED_BOLD, "%s", RESET);
+        final String SUCCESS_MSG = String.format("\t%s%s%s\n", COLOR_GREEN_BOLD, "%s", RESET);
+
+        String screen = DASHBOARD;
+
+        final String APP_TITLE = String.format("%s%s%s",
+            COLOR_BLUE_BOLD, screen, RESET);
+
+            System.out.println(CLEAR);
+            System.out.println("\t" + APP_TITLE + "\n");
+
+
+
+
     }
 }
