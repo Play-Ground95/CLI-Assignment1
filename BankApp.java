96753deecbb17757ad315f8408fb54df4991b121
Kavindu95 <kavindueng95@gmail.com>
2023-08-15 23:27:06 +0530

    add switch

--- BankApp.java
+++ BankApp.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public class BankApp{
+     private static final Scanner SCANNER = new Scanner(System.in);
     public static void main(String[] args) {
         final String CLEAR = "\033[H\033[2J";
         final String COLOR_BLUE_BOLD = "\033[34;1m";
@@ -8,8 +11,12 @@ public class BankApp{
 
         final String DASHBOARD = "👷 Welcome To Smart Banking System";
         final String ADD_ACCOUNT = "➕ Add New Account";
+        final String DEPOSIT = "➕ Deposit";
+        final String WITHDRAW = "➕ Withdraw";
+        final String TRANSFER = "➕ Transfer";
+         final String PRINT_BALANCE = "🖨 Print Balance";
         final String DELETE_ACCOUNT = "❌ Remove Exisiting Account";
-        final String PRINT_BALANCE = "🖨 Print Balance";
+       
 
         final String ERROR_MSG = String.format("\t%s%s%s\n", COLOR_RED_BOLD, "%s", RESET);
         final String SUCCESS_MSG = String.format("\t%s%s%s\n", COLOR_GREEN_BOLD, "%s", RESET);
@@ -22,6 +29,32 @@ public class BankApp{
             System.out.println(CLEAR);
             System.out.println("\t" + APP_TITLE + "\n");
 
+            switch(screen){
+                case DASHBOARD: 
+                    System.out.println("\t[1]. Open New Account");
+                    System.out.println("\t[2]. Deposit Money");
+                    System.out.println("\t[3]. Withdraw Money");
+                    System.out.println("\t[4]. Transfer Money");
+                    System.out.println("\t[5]. Check Account Balance");
+                    System.out.println("\t[6]. Drop Existing Account");
+                    System.out.println("\t[7]. Exit");
+                    System.out.print("\tEnter an option to continue: ");
+                    
+                    int option = SCANNER.nextInt();
+                    SCANNER.nextLine();
+
+                    switch (option){
+                        case 1: screen = ADD_ACCOUNT; break;
+                        case 2: screen = DEPOSIT; break;
+                        case 3: screen = WITHDRAW; break;
+                        case 4: screen = TRANSFER; break;
+                        case 5: screen = PRINT_BALANCE; break;
+                        case 6: screen = DELETE_ACCOUNT; break;
+                        case 7: System.out.println(CLEAR); System.exit(0);
+                        default:
+                    }
+                    break;
+                }
 
 
 
